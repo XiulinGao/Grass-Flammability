6ea3004e7f8006c6e7aa1341d94330bdfa219e7a
Dylan W. Schwilk <dylan@schwilk.org>
2016-09-23 15:28:25 -0500

    Modify HOBO reading code to handle 2016 format
    `2016-burns.csv` was in a different format (end times column, different
    column names) than the 2015 data. The temperature summary code now works
    with this data but it will not work with 2015 now. Best solution is
    probably to modify the 2015 trials file to use the new format.

--- scripts/hobo-temps.R
+++ scripts/hobo-temps.R
@@ -9,18 +9,10 @@ library(tidyr)
 TZ = "CST6CDT"
 
 trials <- read.csv("../data/2016-burns.csv", stringsAsFactors=FALSE) 
-# unfortunately, there are no "ending times" in this file so this is a bit
-# tricky. This data is fragile because of this oversight: we must assume that
-# the order of observations tells us something about end times.
 trials <- trials %>% mutate(start.time = mdy_hm(str_c(trial.date, " ", start.t), tz=TZ)) %>%
-  mutate(end.time = mdy_hm(str_c(trial.date, " ", end.t), tz=TZ))
-          
-#PAD_TIME <- duration(1, units="minutes")
-#MAX_DURATION <- duration(2, units="hours")
-#trials$end.time <- c(trials$start.time[2:length(trials$start.time)] - PAD_TIME,
-                     #trials$start.time[length(trials$start.time)] + MAX_DURATION)
-#trials$end.time[trials$end.time - trials$start.time > MAX_DURATION] <-
-    #trials$start.time[trials$end.time - trials$start.time > MAX_DURATION] + MAX_DURATION
+  mutate(end.time = mdy_hm(str_c(trial.date, " ", end.t), tz=TZ),
+         trial.id = paste(trial.date, trial.num, sep="_"))
+
 trials$interval <- interval(trials$start.time, trials$end.time)
 
 read_hobo_file <- function(filename) {
@@ -65,146 +57,25 @@ thermocouples.wide <- basea %>% full_join(baseb) %>% full_join(height10) %>%
 get_trial_id <- function(time) {
     matches <- time %within% trials$interval
     if(! any(matches)) return(NA)
-    return(trials$trial[which.max(matches)])
+    return(trials$trial.id[which.max(matches)])
 }
 
 # assign trial ids
-thermocouples.wide$trial <- unlist(sapply(thermocouples.wide$time, get_trial_id)) #can't work here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+thermocouples.wide$trial.id <- unlist(sapply(thermocouples.wide$time, get_trial_id))
 
 # throw away data outside of a trial
-thermocouples.wide <- thermocouples.wide %>% filter(! is.na(trial))
+thermocouples.wide <- thermocouples.wide %>% filter(! is.na(trial.id))
 
 # Long form data porbably more useful for per thermcouple summaries.
-thermocouples.long <- thermocouples.wide %>% gather(location, temperature, -time, -trial)
+thermocouples.long <- thermocouples.wide %>% gather(location, temperature, -time, -trial.id)
 
 ## then do the summary
 threshold=60 # temperature threshold in degrees C
-temps.sum <- thermocouples.long %>% group_by(trial, location) %>%
+temps.sum <- thermocouples.long %>% group_by(trial.id, location) %>%
     summarise(dur = sum(temperature > threshold),
               degsec = sum(temperature[temperature > threshold]),
-              peak.temp = max(temperature),
-              peak.time = time[which.max(temperature)],
+              peak.temp = max(temperature, na.rm=TRUE),
+              peak.time = time[which(peak.temp == temperature)[1]],
               num.NA = sum(is.na(temperature))) %>%
     full_join(trials)
 
