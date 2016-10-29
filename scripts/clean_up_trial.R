84c4b6b4560cedb2aaead9ce2b1aa8f7daf79a08
Xiulin Gao <xiulin.gao@ttu.edu>
2016-10-29 16:45:29 -0500

    add script for cleaning up 2016-burns.csv data

---
+++ scripts/clean_up_trial.R
@@ -0,0 +1,33 @@
+###clean_up_trial.R
+###this scrips is excused to clean up recorded trial data
+###main mission is to drop failed duplicated trial and also
+###do basic summary for each trial: time duration for each burn
+###total above ground biomass.
+
+library(dplyr)
+library(lubridate)
+library(stringr)
+
+TZ = "CST6CDT"
+
+trials <- read.csv("../data/2016-burns.csv", stringsAsFactors=FALSE,
+                   na.strings = c("", "N/A")) 
+trials <- trials %>% mutate(temp = (temp-32)*5/9) %>% 
+  filter(!sp.cd=="ARPU9") %>%
+  mutate(total.mass = initial.mass-final.mass) %>%
+  mutate(start.time = mdy_hm(str_c(trial.date, " ",
+                                   start.t), tz=TZ)) %>%
+  mutate(end.time = mdy_hm(str_c(trial.date, " ", end.t), tz=TZ),
+         trial.id = paste(trial.date, trial.num, sep="_"))
+
+#drop duplicated trial, always the first one failed
+#find the duplicated trial id
+n1 <- trials$trial.date[duplicated(trials$label)]
+n2 <- trials$trial.num[duplicated(trials$label)] -1 #the one needs to be droped is 
+#the first one
+dropid <- paste(n1, n2, sep="_")
+#drop the rows of duplicated trials
+trials <- trials %>% filter(!trial.id %in% dropid)
+
+trials$interval <- interval(trials$start.time, trials$end.time)
+
