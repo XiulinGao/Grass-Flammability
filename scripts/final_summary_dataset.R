d0fcc0c72563528d01444c1632e6bc5c19ca1bd2
Xiulin Gao <xiulin.gao@ttu.edu>
2016-11-02 00:17:21 -0500

    this is the scripts for creating fianl data set which contains peak temp. summaries, canopy data, and biomass loss rate for each trial

---
+++ scripts/final_summary_dataset.R
@@ -0,0 +1,28 @@
+###final_summary_dataset.R
+###grab summarize for peak temp, max. biomass loss rate per trial, 
+###architecture data for <10cm and >10cm sections, the create data frame contain 
+###summaries for peak temp., mass loss rate and biomass density for each trial
+
+library(dplyr)
+
+source("./hobo-temps.R") #grab peak temp summaries for each trial:temps.sum
+source("./burning_trial_summaries.R") #grab mass loss rate for each trial:
+                                      #flamingModsCoef
+source("./biomass_density_prediction.R")# architecture data
+
+all_burns <- left_join(temps.sum, flamingModsCoef) %>%
+   left_join(mass_density)
+
+# find all_burns arr missing some canopy data,which should not happen
+# since canopy data should be measred for every single trial,check it out.
+#cause is wrong data entering for pair of erc29 and species code of cs39
+#, thus when they are joined by pair and species code, it gets lost. fixed
+
+
+  
+
+
+
+
+
+
