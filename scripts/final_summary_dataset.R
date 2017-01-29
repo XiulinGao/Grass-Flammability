b69f9a89471695def5762a7261eddad93932e44c
Xiulin Gao <xiulin.gao@ttu.edu>
2017-01-29 13:42:05 -0600

    added code for final summary dataset cleaning up

--- scripts/final_summary_dataset.R
+++ scripts/final_summary_dataset.R
@@ -10,29 +10,72 @@ source("./burning_trial_summaries.R") #grab mass loss rate for each trial:
                                       #flamingModsCoef
 source("./biomass_density_prediction.R")# architecture data
 
-all_burns <- left_join(temps.sum, flamingModsCoef) %>%
-   left_join(mass_density)
+#throw away intervals from trials and tempsec.sum, since it 
+#does not work when join trials with other dataset(can't open file),  
+#and it is also not necessary to keep it
 
-#filter trials that actually have positive biomass 'loss' rate
-#and log total mass
-all_burns <- all_burns %>% filter(estimate<0) %>%
-  mutate(logtmass=log10(total.mass))
+fix.trials <- trials[, -which(names(trials)=="interval")]
+fix.tempsec.sum <- tempsec.sum[, -which(names(tempsec.sum)=="interval")]
 
-temp_balance <- filter(all_burns, !is.na(peak.temp))#throw away dataset with no 
-#temperature data 
+#1. data frame has temp summary for above ane below section, as well
+#as contain both flaming and smoldering biomass loss rate, trial records
+#and canopy
 
-#subset data for sec <10 and >10, mainly is used for temp data
-temp10 <- filter(temp_balance, location %in% c('base', 'height.10')) 
-temp <- filter(temp_balance, location %in% c('height.20', 'height.40'))
+flam.alldata <- left_join(fix.trials, fix.tempsec.sum) %>% 
+  left_join(flamingModsCoef) %>% left_join(mass.density)
+
+smog.alldata <- left_join(fix.trials, fix.tempsec.sum) %>%
+  left_join(smolderingModsCoef) %>% left_join(mass.density)
+
+#2.datasets only contain mass loss rate, trial records and canopy traits
+#this dataset can be used for analyze how traits influence mass loss rate
+#include two: flaming and smoldering
+
+flam.loss <- left_join(fix.trials, flamingModsCoef) %>% 
+  left_join(mass.density) %>% filter(! is.na(lossrate)) 
+
+smog.loss <- left_join(fix.trials, smolderingModsCoef) %>%
+  left_join(mass.density) %>% filter(! is.na(lossrate)) 
+
+#3.datasets only contain temperature relative summary, trial records
+# and canopy traits, sections (<10 and >10 sections) are seperated
+temp.abovesec <- left_join(fix.trials, fix.tempsec.sum) %>%
+  filter(location=="above.sec") %>% left_join(mass.density)
+
+temp.belowsec <- left_join(fix.trials, fix.tempsec.sum) %>%
+  filter(location=="below.sec") %>% left_join(mass.density)
+
+#4. split flam.alldata and smog.alldata into subsets
+#for only flaming stage with available temp and mass loss  
+#data for above sec;smoldering stage with available temp and
+#mass loss data for below sec; 
+
+flamtemp.above <- filter(flam.alldata, location=="above.sec") %>%
+  filter(! is.na(dur)) %>% filter(! is.na(lossrate)) 
 
 
+smogtemp.below <- filter(smog.alldata, location=="below.sec") %>%
+  filter(! is.na(dur)) %>% filter(! is.na(lossrate)) 
 
+#5.dataset only contains burn record and biomass density, mainly for 
+#exploring how total biomass and mass density influence max. flame height
 
+flam.height <- left_join(fix.trials, mass.density)
 
-  
+#6.dataset only contains flammability measures for pca, split by sections
 
+flamabove.pca <- flam.alldata %>% filter(location == "above.sec") %>%
+  subset(select = c("sp.cd", "label", "trial.id", "dur", "degsec",  
+                    "peak.temp", "lossrate", "max.fh")) %>% filter(!is.na(lossrate))
 
+flambelow.pca <- flam.alldata %>% filter(location == "below.sec") %>%
+  subset(select = c("sp.cd", "label", "trial.id", "dur", "degsec",  
+                    "peak.temp", "lossrate", "max.fh")) %>% filter(!is.na(lossrate))
 
+smogabove.pca <- smog.alldata %>% filter(location == "above.sec") %>%
+  subset(select = c("sp.cd", "label", "trial.id", "dur", "degsec", 
+                    "peak.temp", "lossrate", "max.fh")) %>% filter(!is.na(lossrate))
 
-
-
+smogbelow.pca <- smog.alldata %>% filter(location == "below.sec") %>%
+  subset(select = c("sp.cd", "label", "trial.id","dur", "degsec", 
+                    "peak.temp", "lossrate", "max.fh")) %>% filter(!is.na(lossrate))
