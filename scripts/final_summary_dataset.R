ddb6ea2d25c888fe1d5eb31c43dee025cdd9ebeb
Xiulin Gao <xiulin.gao@ttu.edu>
2017-04-23 22:55:36 -0500

    added coed to make dataframe for canopy-trial, canopy-lossrate for different analysis goal

--- scripts/final_summary_dataset.R
+++ scripts/final_summary_dataset.R
@@ -39,52 +39,15 @@ flam.below <- flam.alldata %>% filter(location=="below.sec") %>%
 #this dataset can be used for analyze how traits influence mass loss rate
 #include mass loss rate for two different stages: flaming and smoldering
 
-#flam.loss <- left_join(fix.trials, flamingNLModsCoef) %>% 
-  #left_join(mass.density) %>% filter(! is.na(lossrate)) 
+flam.loss <- left_join(fix.trials, flamlossr) %>% 
+  left_join(mass.density) %>% filter(! is.na(lossrate)) 
 
 #smog.loss <- left_join(fix.trials, smolderingModsCoef) %>%
   #left_join(mass.density) %>% filter(! is.na(lossrate)) 
 
-#3.datasets only contain temperature summary, trial records
-# and canopy traits, split into sections (<10 and >10 sections) 
 
-#temp.abovesec <- left_join(fix.trials, fix.tempsec.sum) %>%
-  #filter(location=="above.sec") %>% left_join(mass.density)
-
-#temp.belowsec <- left_join(fix.trials, fix.tempsec.sum) %>%
-  #filter(location=="below.sec") %>% left_join(mass.density)
-
-#4. split flam.alldata and smog.alldata into subsets
-#for only flaming stage with available temp and mass loss  
-#data for above sec;smoldering stage with available temp and
-#mass loss data for below sec; 
-
-#flamtemp.above <- filter(flam.alldata, location=="above.sec") %>%
-  #filter(! is.na(dur)) %>% filter(! is.na(lossrate)) 
-
-
-#smogtemp.below <- filter(smog.alldata, location=="below.sec") %>%
-  #filter(! is.na(dur)) %>% filter(! is.na(lossrate)) 
-
-#5.dataset only contains burn record and biomass density, mainly for 
+#3.dataset only contains burn record and biomass density, mainly for 
 #exploring how total biomass and mass density influence max. flame height
 
-#flam.height <- left_join(fix.trials, mass.density)
+arc.trial <- left_join(fix.trials, mass.density)
 
-#6.dataset only contains flammability measures for pca, split by sections
-
-#flamabove.pca <- flam.alldata %>% filter(location == "above.sec") %>%
-  #subset(select = c("sp.cd", "label", "trial.id", "dur", "degsec",  
-                    #"peak.temp", "lossrate", "max.fh")) %>% filter(!is.na(lossrate))
-
-#flambelow.pca <- flam.alldata %>% filter(location == "below.sec") %>%
-  #subset(select = c("sp.cd", "label", "trial.id", "dur", "degsec",  
-                    #"peak.temp", "lossrate", "max.fh")) %>% filter(!is.na(lossrate))
-
-#smogabove.pca <- smog.alldata %>% filter(location == "above.sec") %>%
-  #subset(select = c("sp.cd", "label", "trial.id", "dur", "degsec", 
-                    #"peak.temp", "lossrate", "max.fh")) %>% filter(!is.na(lossrate))
-
-#smogbelow.pca <- smog.alldata %>% filter(location == "below.sec") %>%
-  #subset(select = c("sp.cd", "label", "trial.id","dur", "degsec", 
-                    #"peak.temp", "lossrate", "max.fh")) %>% filter(!is.na(lossrate))
