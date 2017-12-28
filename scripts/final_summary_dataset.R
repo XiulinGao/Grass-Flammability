1934b57f7874074270b381805ffa5aa3ca5273b9
Xiulin Gao <xiulin.gao@ttu.edu>
2017-12-28 10:54:49 -0600

    undo previous change of graying one line code out

--- scripts/final_summary_dataset.R
+++ scripts/final_summary_dataset.R
@@ -15,13 +15,13 @@ source("./biomass_density_prediction.R")# architecture data
 
 trials <- select(trials, -interval)
 trials.orga <- trials[order(trials$label), ]
-tempsec.sum <- select(tempsec.sum, -interval)
+tempsec.sum <- select(tempsec.sum, -interval) 
 
 #1. data frame has temp summary for above ane below section, as well
 #as contain both flaming and smoldering biomass loss rate, trial records
 #and canopy
 
-temp.alldata <- left_join(trials.orga, tempsec.sum) %>% 
+temp.alldata <- left_join(trials, tempsec.sum) %>% 
   left_join(flamlossr) %>% left_join(mass.density)
 
 temp.above <- temp.alldata %>% filter(location=="above.sec") %>%
@@ -39,7 +39,7 @@ temp.below <- temp.alldata %>% filter(location=="below.sec") %>%
 #this dataset can be used for analyze how traits influence mass loss rate
 #include mass loss rate for two different stages: flaming and smoldering
 
-flam.loss <- left_join(trials.orga, flamlossr) %>% 
+flam.loss <- left_join(trials, flamlossr) %>% 
   left_join(mass.density) %>% filter(! is.na(lossrate)) 
 
 #smog.loss <- left_join(fix.trials, smolderingModsCoef) %>%
@@ -49,7 +49,7 @@ flam.loss <- left_join(trials.orga, flamlossr) %>%
 #3.dataset only contains burn record and biomass density, mainly for 
 #exploring how total biomass and mass density influence max. flame height
 
-arc.trial <- left_join(trials.orga, mass.density)
+arc.trial <- left_join(trials, mass.density)
 
 #clean up dataset not needed
 rm("balance_data", "balance_sum", "bytrial", "flamburn", "flambytrial", 
