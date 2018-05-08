0530246879a91c14cbfb222023dee6efd440970a
Dylan W. Schwilk <dylan@schwilk.org>
2018-05-08 15:43:21 -0500

    Remove duplicated code in hobo-temps.R
    Now the script exports one data frame: temps.sum

--- scripts/hobo-temps.R
+++ scripts/hobo-temps.R
@@ -25,7 +25,6 @@ concat_hobo_files <- function(filelist, label){
     return(r)
 }
 
-
 # get sets for each of four thermocouple locations
 basea <- concat_hobo_files(list.files("../data/hobo",
                                       full.names=TRUE, pattern = "*0a.csv"), "base.A")
@@ -38,12 +37,15 @@ height20 <- concat_hobo_files(list.files("../data/hobo",
 height40 <- concat_hobo_files(list.files("../data/hobo",
                                          full.names=TRUE, pattern = "*40.csv"), "height.40")
 
+## DWS: ok, but better practice when storing data in file names is to extract
+## column of id variables from file names vector.
+
 # Now merge all of these together to get one continuous time series (wide
 # data). Do we even need this? Really only necessary if we ever compare temps
 # across thermocouples.
 thermocouples.wide <- basea %>% full_join(baseb) %>% 
                       full_join(height10) %>% 
-                      full_join(height20) %>% full_join(height40) 
+                      full_join(height20) %>% full_join(height40)
 
 #take average temp for base, then get ave for below and above 10cm
 thermocouples.wide$base <- rowMeans(subset(thermocouples.wide, select= c(base.A, base.B)),
@@ -54,12 +56,12 @@ thermocouples.wide$base <- rowMeans(subset(thermocouples.wide, select= c(base.A,
 thermocouples.wide$above.sec <- rowMeans(subset(thermocouples.wide, select=c(height.10,
                                     height.20,height.40)), na.rm=TRUE)
 
+## DWS: names here are confusing why is one "base" and one "above.sec". SHould
+## be "base" and "canopy" or "below" and "above", yes?
+
 thermocouples.wide <- thermocouples.wide %>% mutate_at(c("base", 
                                                          "above.sec"),funs(round(.,2)))
-                                                         
-thermocouples.sec <- thermocouples.wide[, -c(2:6)]
 thermocouples.wide <- thermocouples.wide[, -c(2:6)]#get rid off base.A, base.B
-                      
 
 ## get a trial ID from a time point
 get_trial_id <- function(time) {
@@ -71,18 +73,17 @@ get_trial_id <- function(time) {
 
 # assign trial ids
 thermocouples.wide$trial.id <- unlist(sapply(thermocouples.wide$time, get_trial_id))
-thermocouples.sec$trial.id <- unlist(sapply(thermocouples.sec$time, get_trial_id))
 #bi21 is dropped off at this step, and I don't know why
+## DWS: I can't figuire out the aboove statement. Itrial ids have the from
+## date_num, so what is "bi21" A plant id? If something is dropped we must
+## solve that.
 
 
 # throw away data outside of a trial
 thermocouples.wide <- thermocouples.wide %>% filter(! is.na(trial.id))
-thermocouples.sec <- thermocouples.sec %>% filter(!is.na(trial.id))
 
-
-# Long form data porbably more useful for per thermcouple summaries.
+# Long form data probably more useful for per thermcouple summaries.
 thermocouples.long <- thermocouples.wide %>% gather(location, temperature, -time, -trial.id)
-thermocouples.sec.long <- thermocouples.sec %>% gather(location, temperature, -time, -trial.id)
 
 ## then do the summary
 threshold=100 # temperature threshold in degrees C
@@ -94,17 +95,11 @@ temps.sum <- thermocouples.long %>% group_by(trial.id, location) %>%
               num.NA = sum(is.na(temperature))) %>%
     full_join(trials)
 
-tempsec.sum <- thermocouples.sec.long %>% group_by(trial.id, location) %>%
-  summarise(dur = sum(temperature > threshold),
-            degsec = sum(temperature[temperature > threshold]),
-            peak.temp = max(temperature, na.rm=TRUE),
-            peak.time = time[which(peak.temp == temperature)[1]],
-            num.NA = sum(is.na(temperature))) %>%
-  full_join(trials) 
 #clean up env
 rm("concat_hobo_files", "get_trial_id", "read_hobo_file", "threshold",
-   "thermocouples.wide", "thermocouples.sec", "thermocouples.long",
-   "thermocouples.sec.long", "height10", "height20", "height40", 
-   "basea", "baseb")
-  
+   "thermocouples.wide", "thermocouples.long", "height10", "height20",
+   "height40", "basea", "baseb")
+
+# exports temps.sum to workspace
+
   
